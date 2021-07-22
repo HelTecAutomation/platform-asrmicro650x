0f2048ca9bf3e57dc4a74c903ea2c8ad10563e22
valeros <valeros08@gmail.com>
2021-07-22 12:48:03 +0300

    Initial support for ASR6601

--- builder/frameworks/arduino.py
+++ builder/frameworks/arduino.py
@@ -31,25 +31,28 @@ env = DefaultEnvironment()
 platform = env.PioPlatform()
 board = env.BoardConfig()
 core = board.get("build.core")
+mcu = board.get("build.mcu", "")
+is_asr6601 = mcu.startswith("asr6601")
+arch = "asr6601" if is_asr6601 else "asr650x"
 
-FRAMEWORK_DIR = platform.get_package_dir("framework-arduinoasrmicro650x")
+FRAMEWORK_DIR = platform.get_package_dir("framework-arduinoasrmicro")
+CORE_DIR = os.path.join(FRAMEWORK_DIR, "cores", core)
 assert os.path.isdir(FRAMEWORK_DIR)
 
 env.Append(
     CPPDEFINES=[
-        ("ARDUINO", 10813),
-        "ARDUINO_ARCH_ASR650X",
-        "__%s__" % board.get("build.mcu").upper(),
-        "__asr650x__",
+        ("ARDUINO", 10815),
+        "ARDUINO_ARCH_%s" % arch.upper(),
+        "__%s__" % mcu.upper(),
+        "__%s__" % arch,
         ("CONFIG_MANUFACTURER", '\\"ASR\\"'),
-        ("CONFIG_DEVICE_MODEL", '\\"6501\\"'),
+        ("CONFIG_DEVICE_MODEL", '\\"%s\\"' % mcu),
         ("CONFIG_VERSION", '\\"v4.0\\"'),
         ("CY_CORE_ID", 0),
         "CONFIG_LORA_USE_TCXO",
         ("F_CPU", "$BOARD_F_CPU"),
         "SOFT_SE",
     ],
-
     CCFLAGS=[
         "-w",
         "-Wall",
@@ -68,12 +71,10 @@ env.Append(
         "-fno-builtin-snprintf",
         "-Wno-strict-aliasing",
     ],
-
     CXXFLAGS=[
         "-fno-exceptions",
         "-fno-rtti",
     ],
-
     LINKFLAGS=[
         "-Os",
         "-Wl,--gc-sections",
@@ -85,52 +86,74 @@ env.Append(
         "-mthumb",
         "-mthumb-interwork",
         "-specs=nano.specs",
-        "-ffat-lto-objects"
+        "-specs=nosys.specs",
+        "-ffat-lto-objects",
     ],
-
-    CPPPATH=[
-        os.path.join(FRAMEWORK_DIR, "cores", core),
-        os.path.join(FRAMEWORK_DIR, "cores", core, "board"),
-        os.path.join(FRAMEWORK_DIR, "cores", core, "board", "src"),
-        os.path.join(FRAMEWORK_DIR, "cores", core, "board", "inc"),
-        os.path.join(FRAMEWORK_DIR, "cores", core, "device", "sx126x"),
-        os.path.join(FRAMEWORK_DIR, "cores", core, "lora"),
-        os.path.join(FRAMEWORK_DIR, "cores", core, "lora", "system"),
-        os.path.join(FRAMEWORK_DIR, "cores", core, "lora", "system", "crypto"),
-        os.path.join(FRAMEWORK_DIR, "cores", core, "port"),
-        os.path.join(FRAMEWORK_DIR, "cores", core, "port", "include"),
-        os.path.join(FRAMEWORK_DIR, "cores", core, "projects"),
-        os.path.join(FRAMEWORK_DIR, "cores", core, "projects", "PSoC4"),
-        os.path.join(FRAMEWORK_DIR, "cores", core, "cores"),
-        os.path.join(FRAMEWORK_DIR, "cores", core, "Serial"),
-        os.path.join(FRAMEWORK_DIR, "cores", core, "Wire"),
-        os.path.join(FRAMEWORK_DIR, "cores", core, "SPI"),
-    ],
-
-    LIBS=[
-        "stdc++",
-        "m"
-    ],
-
-    LIBSOURCE_DIRS=[
-        os.path.join(FRAMEWORK_DIR, "libraries")
-    ]
+    LIBS=["stdc++", "m"],
+    LIBSOURCE_DIRS=[os.path.join(FRAMEWORK_DIR, "libraries")],
 )
 
 env.Prepend(
     ASFLAGS=env.get("CCFLAGS", [])[:],
-    _LIBFLAGS='"%s" ' % os.path.join(
-        FRAMEWORK_DIR, "cores", core, "projects", "CubeCellLib.a")
+    _LIBFLAGS='"%s" '
+    % (
+        os.path.join(CORE_DIR, "asr6601.a")
+        if is_asr6601
+        else os.path.join(CORE_DIR, "projects", "CubeCellLib.a")
+    ),
 )
+
+if is_asr6601:
+    env.Append(
+        CPPPATH=[
+            CORE_DIR,
+            os.path.join(CORE_DIR, "drivers", "peripheral", "inc"),
+            os.path.join(CORE_DIR, "drivers", "crypto", "inc"),
+            os.path.join(CORE_DIR, "platform", "CMSIS"),
+            os.path.join(CORE_DIR, "platform", "system"),
+            os.path.join(CORE_DIR, "lora", "driver"),
+            os.path.join(CORE_DIR, "lora", "radio"),
+            os.path.join(CORE_DIR, "lora"),
+            os.path.join(CORE_DIR, "lora", "radio", "sx126x"),
+            os.path.join(CORE_DIR, "lora", "system"),
+            os.path.join(CORE_DIR, "lora", "system", "crypto"),
+            os.path.join(CORE_DIR, "base"),
+            os.path.join(CORE_DIR, "peripheral"),
+        ],
+    )
+else:
+    env.Append(
+        CPPPATH=[
+            CORE_DIR,
+            os.path.join(CORE_DIR, "board"),
+            os.path.join(CORE_DIR, "board", "src"),
+            os.path.join(CORE_DIR, "board", "inc"),
+            os.path.join(CORE_DIR, "device", "sx126x"),
+            os.path.join(CORE_DIR, "lora"),
+            os.path.join(CORE_DIR, "lora", "system"),
+            os.path.join(CORE_DIR, "lora", "system", "crypto"),
+            os.path.join(CORE_DIR, "port"),
+            os.path.join(CORE_DIR, "port", "include"),
+            os.path.join(CORE_DIR, "projects"),
+            os.path.join(CORE_DIR, "projects", "PSoC4"),
+            os.path.join(CORE_DIR, "cores"),
+            os.path.join(CORE_DIR, "Serial"),
+            os.path.join(CORE_DIR, "Wire"),
+            os.path.join(CORE_DIR, "SPI"),
+        ],
+    )
+
 
 if not board.get("build.ldscript", ""):
     env.Append(
         LIBPATH=[
-            os.path.join(FRAMEWORK_DIR, "cores", core, "projects", "PSoC4"),
+            CORE_DIR if is_asr6601 else os.path.join(CORE_DIR, "projects", "PSoC4"),
         ]
     )
     env.Replace(
-        LDSCRIPT_PATH=board.get("build.arduino.ldscript", "cm0plusgcc.ld")
+        LDSCRIPT_PATH=board.get(
+            "build.arduino.ldscript", "gcc.ld" if is_asr6601 else "cm0plusgcc.ld"
+        )
     )
 
 #
@@ -146,19 +169,31 @@ env.Append(
         "REGION_%s" % region,
         ("ACTIVE_REGION", "LORAMAC_REGION_%s" % region),
         ("LORAWAN_CLASS", lorawan_config.get("class", "CLASS_A")),
-        ("LORAWAN_NETMODE", "true" if lorawan_config.get(
-            "netmode", "OTAA") == "OTAA" else "false"),
+        (
+            "LORAWAN_NETMODE",
+            "true" if lorawan_config.get("netmode", "OTAA") == "OTAA" else "false",
+        ),
         ("LORAWAN_ADR", "true" if lorawan_config.get("adr", "ON") == "ON" else "false"),
-        ("LORAWAN_UPLINKMODE", "true" if lorawan_config.get(
-            "uplinkmode", "CONFIRMED") == "CONFIRMED" else "false"),
-        ("LORAWAN_NET_RESERVE", "true" if lorawan_config.get(
-            "net_reserve", "OFF") == "ON" else "false"),
+        (
+            "LORAWAN_UPLINKMODE",
+            "true"
+            if lorawan_config.get("uplinkmode", "CONFIRMED") == "CONFIRMED"
+            else "false",
+        ),
+        (
+            "LORAWAN_NET_RESERVE",
+            "true" if lorawan_config.get("net_reserve", "OFF") == "ON" else "false",
+        ),
         ("AT_SUPPORT", 1 if lorawan_config.get("at_support", "ON") == "ON" else 0),
-        ("LORAWAN_DEVEUI_AUTO", 0 if lorawan_config.get("deveui", "CUSTOM") == "CUSTOM" else 1),
-        ("LoraWan_RGB", 1 if lorawan_config.get(
-            "rgb", "ACTIVE") == "ACTIVE" else 0),
-        ("LoRaWAN_DEBUG_LEVEL", 2 if debug_level == "FREQ_AND_DIO" else (
-            1 if debug_level == "FREQ" else 0))
+        (
+            "LORAWAN_DEVEUI_AUTO",
+            0 if lorawan_config.get("deveui", "CUSTOM") == "CUSTOM" else 1,
+        ),
+        ("LoraWan_RGB", 1 if lorawan_config.get("rgb", "ACTIVE") == "ACTIVE" else 0),
+        (
+            "LoRaWAN_DEBUG_LEVEL",
+            2 if debug_level == "FREQ_AND_DIO" else (1 if debug_level == "FREQ" else 0),
+        ),
     ]
 )
 
@@ -168,28 +203,31 @@ env.Append(
 
 libs = []
 
-if "build.variant" in env.BoardConfig():
-    variants_dir = os.path.join(
-        "$PROJECT_DIR", board.get("build.variants_dir")) if board.get(
-            "build.variants_dir", "") else os.path.join(FRAMEWORK_DIR, "variants")
-    env.Append(
-        CPPPATH=[
-            os.path.join(variants_dir, board.get("build.variant"))
-        ]
+if "build.variant" in board:
+    variants_dir = (
+        os.path.join("$PROJECT_DIR", board.get("build.variants_dir"))
+        if board.get("build.variants_dir", "")
+        else os.path.join(FRAMEWORK_DIR, "variants")
     )
-    libs.append(env.BuildLibrary(
-        os.path.join("$BUILD_DIR", "FrameworkArduinoVariant"),
-        os.path.join(variants_dir, board.get("build.variant"))
-    ))
+    env.Append(CPPPATH=[os.path.join(variants_dir, board.get("build.variant"))])
+    libs.append(
+        env.BuildLibrary(
+            os.path.join("$BUILD_DIR", "FrameworkArduinoVariant"),
+            os.path.join(variants_dir, board.get("build.variant")),
+        )
+    )
 
-libs.append(env.BuildLibrary(
-    os.path.join("$BUILD_DIR", "FrameworkArduino"),
-    os.path.join(FRAMEWORK_DIR, "cores", core),
-    src_filter=[
-        "+<*>",
-        "-<projects/PSoC4/CyBootAsmIar.s>",
-        "-<projects/PSoC4/CyBootAsmRv.s>"
-    ]
-))
+libs.append(
+    env.BuildLibrary(
+        os.path.join("$BUILD_DIR", "FrameworkArduino"),
+        CORE_DIR,
+        # Only applicable to ASR6501
+        src_filter=[
+            "+<*>",
+            "-<projects/PSoC4/CyBootAsmIar.s>",
+            "-<projects/PSoC4/CyBootAsmRv.s>",
+        ],
+    )
+)
 
 env.Prepend(LIBS=libs)
--- builder/main.py
+++ builder/main.py
@@ -15,7 +15,12 @@
 from os.path import join
 
 from SCons.Script import (
-    AlwaysBuild, Builder, COMMAND_LINE_TARGETS, Default, DefaultEnvironment)
+    AlwaysBuild,
+    Builder,
+    COMMAND_LINE_TARGETS,
+    Default,
+    DefaultEnvironment,
+)
 
 
 env = DefaultEnvironment()
@@ -31,40 +36,49 @@ env.Replace(
     OBJCOPY="arm-none-eabi-objcopy",
     RANLIB="arm-none-eabi-gcc-ranlib",
     SIZETOOL="arm-none-eabi-size",
-
     ARFLAGS=["rcs"],
-
     SIZEPROGREGEXP=r"^(?:\.text|\.data|\.rodata|\.text.align|\.ARM.exidx|\.cybootloader)\s+(\d+).*",
     SIZEDATAREGEXP=r"^(?:\.data|\.bss|\.noinit)\s+(\d+).*",
     SIZECHECKCMD="$SIZETOOL -A -d $SOURCES",
-    SIZEPRINTCMD='$SIZETOOL -B -d $SOURCES',
-
-    PROGSUFFIX=".elf"
+    SIZEPRINTCMD="$SIZETOOL -B -d $SOURCES",
+    PROGSUFFIX=".elf",
 )
 
 env.Append(
     BUILDERS=dict(
         ElfToHex=Builder(
-            action=env.VerboseAction(" ".join([
-                "$OBJCOPY",
-                "-O",
-                "ihex",
-                "$SOURCES",
-                "$TARGET"
-            ]), "Building $TARGET"),
-            suffix=".hex"
+            action=env.VerboseAction(
+                " ".join(["$OBJCOPY", "-O", "ihex", "$SOURCES", "$TARGET"]),
+                "Building $TARGET",
+            ),
+            suffix=".hex",
+        ),
+        ElfToBin=Builder(
+            action=env.VerboseAction(
+                " ".join(["$OBJCOPY", "-O", "binary", "$SOURCES", "$TARGET"]),
+                "Building $TARGET",
+            ),
+            suffix=".bin",
         ),
         GenerateCyacd=Builder(
-            action=env.VerboseAction(" ".join([
-                '"%s"' % join(platform.get_package_dir(
-                    "tool-cubecellelftool") or "", "CubeCellelftool"),
-                "$OBJCOPY",
-                "${SOURCES[0]}",
-                "${SOURCES[1]}",
-                "$TARGET"
-            ]), "Building $TARGET"),
-            suffix=".cyacd"
-        )
+            action=env.VerboseAction(
+                " ".join(
+                    [
+                        '"%s"'
+                        % join(
+                            platform.get_package_dir("tool-cubecellelftool") or "",
+                            "CubeCellelftool",
+                        ),
+                        "$OBJCOPY",
+                        "${SOURCES[0]}",
+                        "${SOURCES[1]}",
+                        "$TARGET",
+                    ]
+                ),
+                "Building $TARGET",
+            ),
+            suffix=".cyacd",
+        ),
     )
 )
 
@@ -77,15 +91,23 @@ if env.get("PROGNAME", "program") == "program":
 #
 
 target_elf = None
+is_asr6601 = env.BoardConfig().get("build.mcu", "").startswith("asr6601")
 if "nobuild" in COMMAND_LINE_TARGETS:
     target_elf = join("$BUILD_DIR", "${PROGNAME}.elf")
     target_hex = join("$BUILD_DIR", "${PROGNAME}.hex")
-    target_firm = join("$BUILD_DIR", "${PROGNAME}.cyacd")
+    target_firm = join(
+        "$BUILD_DIR", "${PROGNAME}" + (".bin" if is_asr6601 else ".cyacd")
+    )
 else:
     target_elf = env.BuildProgram()
     target_hex = env.ElfToHex(join("$BUILD_DIR", "${PROGNAME}"), target_elf)
-    target_firm = env.GenerateCyacd(
-        join("$BUILD_DIR", "${PROGNAME}"), [target_elf, target_hex])
+    target_firm = (
+        env.ElfToBin(join("$BUILD_DIR", "${PROGNAME}"), target_elf)
+        if is_asr6601
+        else env.GenerateCyacd(
+            join("$BUILD_DIR", "${PROGNAME}"), [target_elf, target_hex]
+        )
+    )
 
 AlwaysBuild(env.Alias("nobuild", target_firm))
 target_buildprog = env.Alias("buildprog", target_firm, target_firm)
@@ -95,8 +117,8 @@ target_buildprog = env.Alias("buildprog", target_firm, target_firm)
 #
 
 target_size = env.Alias(
-    "size", target_elf,
-    env.VerboseAction("$SIZEPRINTCMD", "Calculating size $SOURCE"))
+    "size", target_elf, env.VerboseAction("$SIZEPRINTCMD", "Calculating size $SOURCE")
+)
 AlwaysBuild(target_size)
 
 #
@@ -107,15 +129,15 @@ upload_protocol = env.subst("$UPLOAD_PROTOCOL")
 
 if upload_protocol == "serial":
     env.Replace(
-        UPLOADER="CubeCellflash",
-        UPLOADERFLAGS=[
-            "-serial", '"$UPLOAD_PORT"'
-        ],
-        UPLOADCMD="$UPLOADER $UPLOADERFLAGS $SOURCES"
+        UPLOADER="flash6601" if is_asr6601 else "CubeCellflash",
+        UPLOADERFLAGS=["-p", '"$UPLOAD_PORT"', "flash"]
+        if is_asr6601
+        else ["-serial", '"$UPLOAD_PORT"'],
+        UPLOADCMD="$UPLOADER $UPLOADERFLAGS $SOURCES",
     )
     upload_actions = [
         env.VerboseAction(env.AutodetectUploadPort, "Looking for upload port..."),
-        env.VerboseAction("$UPLOADCMD", "Uploading $SOURCE")
+        env.VerboseAction("$UPLOADCMD", "Uploading $SOURCE"),
     ]
 
 elif upload_protocol == "custom":
